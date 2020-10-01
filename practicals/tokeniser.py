e53edecd453b3f64c27057ae45615025f54dfa3d
isabelhamilton <ih4043a@student.american.edu>
2020-10-01 18:58:34 -0400

    add files

---
+++ practicals/tokeniser.py
@@ -0,0 +1,25 @@
+import sys, re
+
+def tokenise(line):
+	line = re.sub(r'([\(\)”?:!;])', r' \g<1> ', line)
+	line = re.sub(r'([^0-9]),', r'\g<1> ,', line)
+	line = re.sub(r',([^0-9])', r',\g<1>', line)
+	line = re.sub(r'  +', ' ', line)
+
+	output = []
+	for token in line.split(' '):
+		if token == '':
+			continue
+		if token[-1] == '.':
+			token = token[:-1] + ' .'
+		output.append(token)
+
+	return ' '.join(output)
+
+line = sys.stdin.readline()
+while line != '':
+	print(tokenise(line.strip('\n')))
+	line = sys.stdin.readline()
+
+
+
