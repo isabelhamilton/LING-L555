40705532d333c0e72ad1f60bb03f6c301a0e6074
isabelhamilton <ih4043a@student.american.edu>
2020-10-27 15:22:14 -0400

    add files

---
+++ practicals/transcribe.py
@@ -0,0 +1,43 @@
+import sys
+
+transcription = {}
+
+transcription['a, A'] = ['a']
+transcription['b, B'] = ['b, β']
+transcription['c, C'] = ['k, s']
+transcription['d, D'] = ['d, ð']
+transcription['e, E'] = ['e']
+transcription['f, F'] = ['f']
+transcription['g, G'] = ['g, ɣ']
+transcription['h, H'] = ['h']
+transcription['i, I'] = ['i']
+transcription['j, J'] = ['j, x']
+transcription['k, K'] = ['k']
+transcription['l, L'] = ['l']
+transcription['m, M'] = ['m']
+transcription['n, N'] = ['n']
+transcription['ñ, Ñ'] = ['ɲ']
+transcription['o, O'] = ['o']
+transcription['p, P'] = ['p']
+transcription['q, Q'] = ['k']
+transcription['r, R'] = ['r, ɾ']
+transcription['s, S'] = ['s']
+transcription['t, T'] = ['t']
+transcription['u, U'] = ['u']
+transcription['v, V'] = ['b, β']
+transcription['w, W'] = ['u']
+transcription['x, X'] = ['ʃ']
+transcription['y, Y'] = ['i']
+transcription['z, Z'] = ['s']
+transcription['dd, DD'] = ['ɟ']
+transcription['ll, LL'] = ['ʎ']
+transcription['ts, TS'] = ['ts']
+transcription['tx, TX'] = ['c']
+transcription['tz, TZ'] = ['tʃ']
+
+
+
+
+
+
+
